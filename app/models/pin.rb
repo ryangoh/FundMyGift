b609de6800800b171ae1524085b564ffae1b8ce0
Ryan Goh <gohengkeat89@gmail.com>
2015-12-18 22:36:39 +0800

    First Commit

---
+++ app/models/pin.rb
@@ -0,0 +1,8 @@
+class Pin < ActiveRecord::Base
+  acts_as_votable
+
+  belongs_to :user
+
+  has_attached_file :image, styles: { medium: "300x300>"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+end
